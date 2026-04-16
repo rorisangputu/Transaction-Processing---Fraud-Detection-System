0567e6b2141d245f3f8919e18010a80be35b5886
Rorisang Putu <rorisangputu@gmail.com>
2026-04-16 23:00:33 +0200

    Transaction Processing & Fraud Detection System

--- BankingSystem.API/obj/Debug/net8.0/BankingSystem.API.AssemblyInfo.cs
+++ BankingSystem.API/obj/Debug/net8.0/BankingSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e09b9c8249ddddec5721f35e08b2e3db248b197")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
